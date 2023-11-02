797880f5d8be038f2ed5cbc0219b596826bd9791
christiandelgadotenesaca <christian4delgado@gmail.com>
2023-11-02 07:18:29 -0500

    Inicio de proyecto

---
+++ build/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var config = {
+  PORT: 3002,
+  SECRET: 'INEXCELSISDEO',
+  MONGODB_URI: 'mongodb+srv://chris:111@cluster0.bxh2rl1.mongodb.net/?retryWrites=true&w=majority'
+};
+module.exports = config;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3002,
+    SECRET:'INEXCELSISDEO',
+    MONGODB_URI:'mongodb+srv://chris:111@cluster0.bxh2rl1.mongodb.net/?retryWrites=true&w=majority'
+}
+
+module.exports = config
